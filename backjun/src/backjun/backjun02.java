50d1325ac58b50e01afda4a52f8a152c9011e562
kwak-daeun <kjkk126@NATE.COM>
2022-12-15 15:59:53 +0900

    백준알고리즘1001

---
+++ backjun/src/backjun/backjun02.java
@@ -0,0 +1,18 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class backjun02 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		System.out.println(A-B);
+	
+
+	}
+
+}
