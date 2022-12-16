f4e584c3360dcf73436cf7360d8da8b12f0cb1a0
kwak-daeun <kjkk126@NATE.COM>
2022-12-16 18:20:20 +0900

    백준 알고리즘

--- backjun/src/b02_조건문/backjun2753.java
+++ backjun/src/b02_조건문/backjun2753.java
@@ -1,5 +1,21 @@
 package b02_조건문;
 
-public class backjun2753 {
+import java.util.Scanner;
 
-}
+public class backjun2753 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int year = scanner.nextInt();
+		
+		if((year%4 ==0) && (year%100 !=0)||(year%400 ==0)) {
+			System.out.print(1);
+		}else {
+				System.out.println(0);
+			}
+			
+		}
+		
+	}
+
+
