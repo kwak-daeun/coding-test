c5fc88b01bfe94e186c6fbc2148bdec104c3e5f8
kwak-daeun <kjkk126@NATE.COM>
2022-12-27 23:29:53 +0900

    아스키코드

---
+++ backjun/src/b06_문자열/backjun11654.java
@@ -0,0 +1,17 @@
+package b06_문자열;
+
+import java.util.Scanner;
+
+public class backjun11654 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		int result = str.charAt(0);
+
+		System.out.println(result);
+	}
+		
+}
