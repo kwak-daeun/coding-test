bc2138a285d099a66d852170bef0aaf33c263633
kwak-daeun <kjkk126@NATE.COM>
2023-01-06 15:57:43 +0900

    다이얼

---
+++ backjun/src/b06_문자열/baekjoon5622.java
@@ -0,0 +1,39 @@
+package b06_문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon5622 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		String s = br.readLine();
+		
+		int[] arr = new int[26];
+		arr[0] = arr[1] = arr[2] = 3; //A,B,C는 각각 3초씩
+		arr[3] = arr[4] = arr[5] = 4; 
+		arr[6] = arr[7] = arr[8] = 5; 
+		arr[9] = arr[10] = arr[11] = 6; 
+		arr[12] = arr[13] = arr[14] = 7; 
+		arr[15] = arr[16] = arr[17] = arr[18] = 8;
+		arr[19] = arr[20] = arr[21] = 9; 
+		arr[22] = arr[23] = arr[24] = arr[25] = 10; 
+		
+		int sum=0; //걸리는 시간
+		for(int i=0; i<s.length(); i++) {
+			char c = s.charAt(i);
+			
+			sum += arr[(int)c - 65]; //아스키코드값 이름
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}
