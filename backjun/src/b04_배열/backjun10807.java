a43683596e406e8f86c8af69eae0788f607d1a2f
kwak-daeun <kjkk126@NATE.COM>
2023-01-09 23:59:14 +0900

    갯수 세기

--- backjun/src/b04_배열/backjun10807.java
+++ backjun/src/b04_배열/backjun10807.java
@@ -1,29 +1,39 @@
 package b04_배열;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class backjun10807 {
 //실패
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = sc.nextInt();
-		int arr[] = new int[n];
+		int n = Integer.parseInt(br.readLine()); 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		//int arr[] = new int[n];//첫쨋줄 정수의 갯수 n개
+		
 		int cnt = 0;	
+	
+//		for(int i=0; i<n; i++) {
+//			arr[i] = Integer.parseInt(st.nextToken());
+//		}
+//		
+		int v = Integer.parseInt(br.readLine()); 
 		
 		for(int i=0; i<n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		int v = sc.nextInt();
-		
-		for(int j=0; j<arr.length; j++) {
-			if(arr[j] ==v) {
+			if(Integer.parseInt(st.nextToken()) == v) {
 				cnt++;
 			}
 			
-			System.out.println(cnt);
 		}
+		
+		System.out.println(cnt);
+		
+		
 		
 
 	}
