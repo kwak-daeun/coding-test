7cd5a27ab00ee1f0c5f0b6d043a0ef553149933d
kwak-daeun <kjkk126@NATE.COM>
2023-01-23 15:15:16 +0900

    A+B-4

--- backjun/src/b03_반복문/backjun10951.java
+++ backjun/src/b03_반복문/backjun10951.java
@@ -1,41 +1,23 @@
 package b03_반복문;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+
+import java.util.Scanner;
+
 
 public class backjun10951 {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+	public static void main(String[] args) {
 		
-		int N = Integer.parseInt(br.readLine());
+		Scanner sc = new Scanner(System.in);
 		
-		StringTokenizer st;
-		int A,B;
+		while(sc.hasNextInt()) {
 		
-		for(int i=1; i<=N; i++) {
-			st = new StringTokenizer(br.readLine());
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
-			
-			bw.write((A+B) + "\n");
-			
+		//두 정수 A,B 입력 받는다.
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A+B);
 			
 		}
 		
-		br.close();
-		bw.flush();
-		bw.close();
-	
-		
-		
-
 	}
-
+	
 }
